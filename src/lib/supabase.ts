3d619e77e6daa46f8e368cbc074f792e52a8e417
StevenJobins <63815966+StevenJobins@users.noreply.github.com>
2026-03-24 17:29:47 +0100

    fix: disable navigator.locks and configure auth properly

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://tziiohzrsyqyqalwiyqe.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6aWlvaHpyc3lxeXFhbHdpeXFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNTE2ODEsImV4cCI6MjA4OTkyNzY4MX0.VOJERvErjjXfbgelpn_-c8EoYFh2bcQJduqbUoo8fPs'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    flowType: 'implicit',
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+    lock: false,
+  }
+})
