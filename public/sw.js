4ae586c96300c5f85e14e0e7d42fbfce67698ced
StevenJobins <63815966+StevenJobins@users.noreply.github.com>
2026-03-26 09:09:04 +0100

    chore: bump SW cache to v13

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tuki-family-v12'
+const CACHE_NAME = 'tuki-family-v13'
 const urlsToCache = ['/tuki-app/', '/tuki-app/index.html']
 
 self.addEventListener('install', (event) => {
