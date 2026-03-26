acdad8a756029ce02a2bbfa6acb9b6ef9c5454a2
StevenJobins <63815966+StevenJobins@users.noreply.github.com>
2026-03-26 19:50:25 +0100

    chore: bump SW cache to v20 for spring content update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tuki-family-v19'
+const CACHE_NAME = 'tuki-family-v20'
 const urlsToCache = ['/tuki-app/', '/tuki-app/index.html']
 
 self.addEventListener('install', (event) => {
