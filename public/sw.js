493e49a92b150d0dbfd9bfafa2e8c288d59261ea
StevenJobins <63815966+StevenJobins@users.noreply.github.com>
2026-03-30 07:50:17 +0200

    chore: bump SW cache to v25 for star shop feature

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tuki-family-v24'
+const CACHE_NAME = 'tuki-family-v25'
 const urlsToCache = ['/tuki-app/', '/tuki-app/index.html']
 
 self.addEventListener('install', (event) => {
