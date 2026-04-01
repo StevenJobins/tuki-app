9094eb1d428c87cf426590c9ce25bd0399dbf070
StevenJobins <63815966+StevenJobins@users.noreply.github.com>
2026-04-01 10:04:12 +0200

    chore: bump SW to v28

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tuki-family-v27'
+const CACHE_NAME = 'tuki-family-v28'
 const urlsToCache = ['/tuki-app/', '/tuki-app/index.html']
 
 self.addEventListener('install', (event) => {
