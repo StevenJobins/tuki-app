b7df34d8fd45358aaed8d313f74c4b7cfc5c28c4
StevenJobins <63815966+StevenJobins@users.noreply.github.com>
2026-05-19 20:12:33 +0200

    feat: add AuthProvider wrapper in main.tsx for Supabase auth

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { AppProvider } from './context/AppContext'
+import { AuthProvider } from './context/AuthContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
+      <AuthProvider>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </AuthProvider>
     </HashRouter>
   </React.StrictMode>,
 )
